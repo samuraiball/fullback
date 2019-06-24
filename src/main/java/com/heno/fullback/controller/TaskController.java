4cc2f23fbb8cee50850c8df75be20cf5457f2fbc
hirooka <yuy.hirohiro@gmail.com>
2019-06-24 22:33:19 +0900

    feature: add a task controller

---
+++ src/main/java/com/heno/fullback/controller/TaskController.java
@@ -0,0 +1,42 @@
+package com.heno.fullback.controller;
+
+import com.heno.fullback.model.entitiy.Task;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class TaskController {
+
+	@GetMapping("/tasks")
+	public Task getAllTask() {
+		return null;
+	}
+
+
+	@GetMapping("/task/{taskId}")
+	public Task getTask(
+			@PathVariable String taskId
+	) {
+		return null;
+	}
+
+	@PostMapping("/task")
+	public ResponseEntity<Task> postTask() {
+		return null;
+	}
+
+	@PutMapping("/task")
+	public Task putTask() {
+		return null;
+	}
+
+	@DeleteMapping("/task/{taskId}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public Task deleteTask(
+			@PathVariable String taskId
+	) {
+		return null;
+	}
+}
